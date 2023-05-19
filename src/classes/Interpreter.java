9392656446f6c99a7f49263b027114825486796c
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-19 18:35:30 +0300

    Started implementing the Interpreter

--- src/classes/Interpreter.java
+++ src/classes/Interpreter.java
@@ -2,6 +2,18 @@ package src.classes;
 
 public class Interpreter {
     public void parseAndExecute(String line){
+        String[] instructionComponents = line.split(" ");
+        switch(instructionComponents[0]) {
+            case "print":
+                System.out.println(instructionComponents[1]);
+            case "assign":
+            case "writeFile":
+            case "readFile":
+            case "printFromTo":
+            case "semWait":
+            case "semSignal":
+        }
+
 
     }
 }

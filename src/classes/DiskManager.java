ff514baf971fdb0cc8c63a16a206f6fd34045c7b
Youssef El Sharkawy <yelsharkawy42@gmail.com>
2023-05-20 02:47:10 +0300

    Added addProcess, finished swap

--- src/classes/DiskManager.java
+++ src/classes/DiskManager.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.Vector;
 
 public class DiskManager {
-    public Vector<Process> disk;
+    public Vector<Process> disk = new Vector<>();
     public static final String dir = "src/main/resources/";
 
     public static void writeFile(String fileName, String data) throws Exception{
@@ -25,7 +25,13 @@ public class DiskManager {
     }
 
     public Process swapProcessFromRam(int pidOnDisk, Process processOnRam){
-
+        disk.add(processOnRam);
+        for(Process p: disk){
+            if(p.pid == pidOnDisk){
+                disk.remove(p);
+                return p;
+            }
+        }
         return null;
     }
     public Process getProcess(int pidOnDisk){
@@ -34,6 +40,10 @@ public class DiskManager {
                 return p;
         }
         return null;
+    }
+
+    public void addProcess(Process newProcess){
+        disk.add(newProcess);
     }
 
     public void serializeProcesses() throws Exception{
@@ -45,13 +55,19 @@ public class DiskManager {
     }
 
     public static void main(String[] args) throws Exception {
-        String y = "j\nj\n\nj";
-        String x;
-        x = readFile("hmm");
-        System.out.println(x);
-        writeFile("hmm", y);
-        x = readFile("hmm");
-        System.out.println(x);
+        Vector<String> l = new Vector<>();
+        l.add("sh");
+        Process p1 = new Process(l, 0);
+        Vector<String> l2= new Vector<>();
+        l2.add("sdf");
+        Process p2 = new Process(l2, 0);
+        DiskManager d = new DiskManager();
+        d.addProcess(p1);
+        Process theP = d.getProcess(0);
+        System.out.println(theP.linesOfCode.get(0));
+        d.swapProcessFromRam(0, p2);
+        theP = d.getProcess(0);
+        System.out.println(theP.linesOfCode.get(0));
     }
 
 }

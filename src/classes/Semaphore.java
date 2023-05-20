7bc48992af4a516844bef06f1adf9cbba448a3cd
Ali Eldin <alieldin82@gmail.com>
2023-05-20 22:14:39 +0300

    Finalized the semWait() method.

--- src/classes/Semaphore.java
+++ src/classes/Semaphore.java
@@ -18,7 +18,7 @@ public class Semaphore {
     }
 
     public static void semSignal(ResourceType resource, int pid) {
-        if (Scheduler.runningPid.val == pid){ //check if the requesting process is the running process
+        if (Scheduler.runningPid.val == pid){ //check if the process letting go of the resource is the running process
             if (resource.equals(ResourceType.userInput) && userInputUsed) {
 
                 //Mark the resource as available
@@ -50,9 +50,40 @@ public class Semaphore {
         }
     }
 
-
     public static boolean semWait(ResourceType resource, int pid){
+        //return flase by default if the process is not the running process
+        if (Scheduler.runningPid.val == pid) {
+
+            // if the resource is available, mark it as used and return true
+            if (resource.equals(ResourceType.userInput) && !userInputUsed) {
+                userInputUsed = true;
+                return true;
+
+            } else if (resource.equals(ResourceType.userOutput) && !userOutputUsed) {
+                userOutputUsed = true;
+                return true;
+
+            } else if (resource.equals(ResourceType.file) && !fileUsed) {
+                fileUsed = true;
+                return true;
+
+                // if the resource is not available, add the process to the blocked queue of that resource and return false
+            } else if (resource.equals(ResourceType.userInput) && userInputUsed) {
+                userInputBlockedQueue.add(pid);
+                return false;
+
+            } else if (resource.equals(ResourceType.userOutput) && userOutputUsed) {
+                userOutputBlockedQueue.add(pid);
+                return false;
+
+            } else if (resource.equals(ResourceType.file) && fileUsed) {
+                fileBlockedQueue.add(pid);
+                return false;
+            }
+        }
         return false;
     }
 
-    }
+
+
+}

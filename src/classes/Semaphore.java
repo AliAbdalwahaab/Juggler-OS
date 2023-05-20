07f65aef3a47895265b765d50b6daab98920d24a
Ali Eldin <alieldin82@gmail.com>
2023-05-20 21:08:59 +0300

    Finalized the semSignal() method and edited static assignments in some classes for better compatibility with semaphore ops.

--- src/classes/Semaphore.java
+++ src/classes/Semaphore.java
@@ -1,19 +1,58 @@
 package src.classes;
 
 public class Semaphore {
-    public SchedulerQueue userInputBlockedQueue;
-    public SchedulerQueue userOutputBlockedQueue;
-    public SchedulerQueue fileBlockedQueue;
-    public boolean userInputUsed;
-    public boolean userOutputUsed;
-    public boolean fileUsed;
+    public static SchedulerQueue userInputBlockedQueue;
+    public static SchedulerQueue userOutputBlockedQueue;
+    public static SchedulerQueue fileBlockedQueue;
+    public static boolean userInputUsed;
+    public static boolean userOutputUsed;
+    public static boolean fileUsed;
 
-
-    public static boolean semSignal(ResourceType resource, int pid) {
-        return true;
+    public Semaphore() {
+        userInputBlockedQueue = new SchedulerQueue();
+        userOutputBlockedQueue = new SchedulerQueue();
+        fileBlockedQueue = new SchedulerQueue();
+        userInputUsed = false;
+        userOutputUsed = false;
+        fileUsed = false;
     }
 
-    public static boolean semWait(ResourceType resource, int pid) {
+    public static void semSignal(ResourceType resource, int pid) {
+        if (Scheduler.runningPid.val == pid){ //check if the requesting process is the running process
+            if (resource.equals(ResourceType.userInput) && userInputUsed) {
+
+                //Mark the resource as available
+                userInputUsed = false; //Mark the resource as available
+
+                //remove Process from the resource blocked queue if it were there after a failed request before
+                userInputBlockedQueue.remove(pid);
+
+                return;
+            } else if (resource.equals(ResourceType.userOutput) && userOutputUsed) {
+
+                //Mark the resource as available
+                userOutputUsed = false;
+
+                //remove Process from the resource blocked queue if it were there after a failed request before
+                userOutputBlockedQueue.remove(pid);
+
+                return;
+
+            } else if (resource.equals(ResourceType.file) && fileUsed) {
+
+                //Mark the resource as available
+                fileUsed = false;
+
+                //remove Process from the resource blocked queue if it were there after a failed request before
+                fileBlockedQueue.remove(pid);
+                return;
+            }
+        }
+    }
+
+
+    public static boolean semWait(ResourceType resource, int pid){
         return false;
     }
-}
+
+    }
